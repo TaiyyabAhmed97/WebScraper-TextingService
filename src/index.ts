c110bb7dc72ad54f450fb1db64dda0523259fb26
Taiyyab <hafiztaiyyab11@gmail.com>
2020-06-15 19:50:38 -0500

    scraped ilmstudios, got namaz object from console

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,25 @@
-console.log("Hello World")
+import puppeteer = require('puppeteer')
+import { isRegExp } from 'util';
+
+let namazTimes: Array<any>;
+(async () => {
+    const browser = await puppeteer.launch()
+    const page = await browser.newPage()
+    const url = 'https://ilmstudios.com/prayer-times'
+
+    page.on('console', msg => {
+        for (let i = 0; i < msg.args().length; ++i) {
+            let logged: any = msg.args()[i]
+            if (logged._remoteObject.value[0] == '[') {
+                let obj = JSON.parse(logged._remoteObject.value)
+                namazTimes = obj
+                console.log(namazTimes)
+            }
+
+        }
+
+
+    });
+    await page.goto(url);
+
+})()
